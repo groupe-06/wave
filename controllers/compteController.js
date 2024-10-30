8249f48f2029c1a36b8a1f7ea61e34a1e7439223
babaissandiaye20 <babaissandiaye31@gmail.com>
2024-10-30 09:41:20 +0000

    solde

--- controllers/compteController.js
+++ controllers/compteController.js
@@ -27,4 +27,21 @@ export const modifyStateAccount = async (req, res) => {
         console.error(error);
         return res.status(500).json({ message: 'Erreur lors de la mise à jour du compte', error });
     }
+    
+};
+export const getCompteByUser = async (req, res) => {
+    const userId = req.userId; // ID de l'utilisateur connecté extrait du token
+
+    try {
+        const compte = await Compte.findOne({ utilisateur: userId });
+        
+        if (!compte) {
+            return res.status(404).json({ message: 'Compte non trouvé pour cet utilisateur' });
+        }
+
+        return res.status(200).json({ message: 'Compte récupéré avec succès', data: compte });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Erreur lors de la récupération du compte', error });
+    }
 };
--- routes/compteRoute.js
+++ routes/compteRoute.js
@@ -1,10 +1,11 @@
 // routes/compteRoute.js
 
 import express from 'express';
-import { modifyStateAccount } from '../controllers/compteController.js';
+import { modifyStateAccount,getCompteByUser } from '../controllers/compteController.js';
 import { getToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
+router.get('/mon-compte', getToken, getCompteByUser);
 
 router.patch('/:compteId/etat', getToken, modifyStateAccount);
 
